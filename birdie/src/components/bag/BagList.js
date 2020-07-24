99a567b2829dc74d47d53637df405885595946b0
stephensenft21 <stephensenft21@gmail.com>
2020-07-24 18:28:13 -0500

    finished writing bagList componente. ready for testing

---
+++ birdie/src/components/bag/BagList.js
@@ -0,0 +1,32 @@
+// populates list of products in individual category
+import React, { useState, useEffect } from "react";
+import API from "../../API/DataManager";
+
+const BagList = (props) => {
+    const [bags, setBags] = useState([]);
+
+    const getBags = () => {
+        return API.getAll().then((response) => {
+            setBags(response);
+        });
+    };
+
+    useEffect(() => {
+        getBags();
+    }, []);
+
+    return (
+        <div className="content">
+            <div>
+                <h2>Your Bags</h2>
+                <div>
+                    {bags.map((bag) => (
+                        <bagListCard key={bag.id} bag={bag} {...props} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BagList;
