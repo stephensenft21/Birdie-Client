170a42aed22fbec9eb2b59245c3b2e6dd52021fa
stephensenft21 <stephensenft21@gmail.com>
2020-07-28 12:12:47 -0500

    fixed issue in bag details, json server data needed to be lower case to return json objects

--- birdie/src/components/bag/BagDetails.js
+++ birdie/src/components/bag/BagDetails.js
@@ -5,14 +5,24 @@ import API from "../../API/DataManager";
 const BagDetail = props => {
     const [bag, setBag] = useState({ brand: "" });
 
-    useEffect(() => {
-        //get(id) from AnimalManager and hang on to the data; put it into state
+
+    const getOneBag = () => {
+        // if((isAuthenticated())){
+
+        // }
+                
         API.getOne("bags",props.bagId)
             .then(bag => {
                 setBag({
                     brand: bag.brand
                 });
             });
+    };
+
+
+
+    useEffect(() => {
+        getOneBag()
     }, [props.bagId]);
 
     return (
